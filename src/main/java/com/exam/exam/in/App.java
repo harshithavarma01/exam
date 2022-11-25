77091f765d9cf5a618f11e265be8dd9456f9cf58
Harshitha Varma <harshithavarma004@gmail.com>
2022-11-25 12:50:25 +0530

    exam project

---
+++ src/main/java/com/exam/exam/in/App.java
@@ -0,0 +1,13 @@
+package com.exam.exam.in;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "This is SE lab internal ! exam" );
+    }
+}
